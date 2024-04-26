12f6c64ad127188e5290313abcf0b469a6d9662a
Eric <ekellogg90@gmail.com>
2024-04-25 22:24:57 -0500

    form validation

--- assets/js/form.js
+++ assets/js/form.js
@@ -13,12 +13,31 @@ formEl.addEventListener('submit', function(event) {
         content: content.value.trim(),
     };
 
+    // Ensures field input
+    if (!userName.value) {
+        alert("Enter a User Name");
+        return false;
+    };
+
+    if (!title.value) {
+        alert("Enter a Title");
+        return false;
+    };
+
+    if (!content.value) {
+        alert("Enter Content");
+        return false;
+    };
+
     let blogArray = JSON.parse(localStorage.getItem('blogArray'));
     if (!blogArray) {
         blogArray = [];
     }
+    
     blogArray.push(blogEntry);
 
     localStorage.setItem('blogArray', JSON.stringify(blogArray));
-    location.href = "blog.html" //maybe later add in error handling for null values
+
+    // Opens to page 2
+    location.href = "blog.html"
 });
